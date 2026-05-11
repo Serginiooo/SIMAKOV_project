cbe106d7b01c71342c5341127b4ca81eca5895db
Serginiooo <153122425+Serginiooo@users.noreply.github.com>
2026-05-11 22:25:36 +0300

    Update task 1.py

--- task 1.py
+++ task 1.py
@@ -1,11 +1,2 @@
-import pandas as pd
-import matplotlib.pyplot as plt
 
-df = pd.read_csv('5 train.csv')
-
-seasonal = df.groupby('season')[['casual', 'registered']].mean()
-
-seasonal.plot(kind='bar')
-plt.xlabel("Время года")
-plt.ylabel("Среднее число поездок")
 
